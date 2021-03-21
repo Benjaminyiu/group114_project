cc13e95aaa5378ae23223ceafe3284ef26ed1e90
Benjaminyiu <78790604+Benjaminyiu@users.noreply.github.com>
2021-03-21 17:12:43 +0800

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-#define SPADE   "\xE2\x99\xA0"
-#define CLUB    "\xE2\x99\xA3"
-#define HEART   "\xE2\x99\xA5"
-#define DIAMOND "\xE2\x99\xA6"
+void main_page(){
+  cout << "--------------------Welcome to Minesweeper!" << endl;
+}
 
 int main(){
-  cout << CLUB << " " << HEART << " " << DIAMOND << endl;
+  
+  main_page();
+  
   return 0;
 }
