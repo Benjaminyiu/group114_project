067ed7b2492da5acc745b46590a0f4af67744991
Benjaminyiu <kcyiuac@connect.hku.hk>
2021-03-21 18:08:07 +0800

    created main page program

--- main_page.cpp
+++ main_page.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
 using namespace std;
 
+#define BOMB "\xF0\x9F\x92\xA3"
+#define BLANK
 void main_page(){
-  cout << "--------------------Welcome to Minesweeper!" << endl;
+  int option = 1;
+
+  cout << "--------------------Welcome to Minesweeper!--------------------" << endl;
+  cout << " ___________________________________ " << endl;
+  cout << "|  " << BOMB << "          " << BOMB << "                   |" << endl;
+  cout << "|                  " << BOMB << "               |" << endl;
+  cout << endl;
+  cout << "1. Play Game" << endl;
+  cout << "2. Instructions" << endl;
+  cout << "3. View Top 10 Players" << endl;
+  cout << "4. Exit Game" << endl << endl;
+
+  cout << "Input 1-4 to continue: ";
+  cin >> option;
+
+  if (option == 1){
+    // init game
+  }
+  else if (option == 2){
+    // display instructions
+  }
+  else if (option == 3){
+    // load ranking from rank.txt
+  }
 }
 
 int main(){
-  
+
   main_page();
-  
+
+  cout << endl << "Bye!" << endl;
+
   return 0;
 }
