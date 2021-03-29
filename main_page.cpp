3450c53ad83d3ca2a8d7ccdf32df0d2923fbe76d
Benjaminyiu <kcyiuac@connect.hku.hk>
2021-03-30 01:40:38 +0800

    Improved indentation and created printBoard function

--- main_page.cpp
+++ main_page.cpp
@@ -15,6 +15,20 @@ const int bomb_easy = 10;
 const int bomb_medium = 40;
 const int bomb_hard = 99;
 
+void printBoard(int b[][size_easy]) {
+    cout << "  0 1 2 3 4 5 6 7 8 9" << endl;
+        for (int i = 0; i < size_easy; i++) {
+            cout << i << " ";
+            for (int j = 0; j < size_easy; j++) {
+                if (b[i][j] == 0)
+                    cout << BOMB << " ";
+                else
+                    cout << "  ";
+            }
+            cout << endl;
+        }
+}
+
 void main_page(){
     int option = 1;
 
@@ -33,46 +47,50 @@ void main_page(){
   	cin >> option;
 
   	if (option == 1){
-		// init game
-		cout << "\n";
-		srand((unsigned) time(0));
+		      // init game
+		    cout << "\n";
+        srand((unsigned) time(0));
 
-		// random seeds for setting mines in the board
-		random_device rd;
-		mt19937 gen(rd());
-		uniform_int_distribution<> dis1(0, 1);
-		uniform_int_distribution<> dis2(1, 2); // limiting mine numbers in a row
+		    // random seeds for setting mines in the board
+		    random_device rd;
+		    mt19937 gen(rd());
+		    uniform_int_distribution<> dis1(0, 1);
+		    uniform_int_distribution<> dis2(1, 2); // limiting mine numbers in a row
 
-		// creating the board, random seeds for setting number of mines
-		int size = 9; // board size e.g. 10 means 10x10
-		int board[size][size];
-		int minmines = 13;	// setting minimum number of mines
-		int mines = rand() % 14 + minmines; // setting number of mines 
+		    // creating the board, random seeds for setting number of mines
+		    int size = 9; // board size e.g. 10 means 10x10
+		    int board[size_easy][size_easy];
+		    int minmines = 13;	// setting minimum number of mines
+		    int mines = rand() % 14 + minmines; // setting number of mines
 
-		ofstream fout("Cheatboard.txt");
-		// setting mines in the board
-		for (int i = 0; i < size; i++) {
-			int lim = dis2(gen); // generating random number between 1 and 2
-			for (int j = 0; j < size; j++) {
-				int ran = dis1(gen);	// generating random number between 0 and 1
-				if (ran == 1 && mines > 0 and lim > 0) {
-					board[i][j] = 0;	// 0 = mine
-					fout << 0 << " ";
-					mines -= 1;
-					lim--;
-				}
-				else {
-					board[i][j] = 1;	// 1 = no mine
-					fout << 1 << " ";
-				}
-			}
-			fout << "\n";
-		}
+		    ofstream fout("Cheatboard.txt");
+		    // setting mines in the board
+		    for (int i = 0; i < size_easy; i++) {
+			      int lim = dis2(gen); // generating random number between 1 and 2
+			      for (int j = 0; j < size_easy; j++) {
+				        int ran = dis1(gen);	// generating random number between 0 and 1
+				        if (ran == 1 && mines > 0 && lim > 0) {
+					          board[i][j] = 0;	// 0 = mine
+					          fout << 0 << " ";
+					          mines -= 1;
+					          lim--;
+				        }
+				        else {
+					          board[i][j] = 1;	// 1 = no mine
+					          fout << 1 << " ";
+				        }
+			      }
+			      fout << "\n";
+
+		    }
+        fout.close();
+
+        printBoard(board);
   	}
 
   	else if (option == 2){
-    	// display instructions
-    	cout << "Each Minesweeper game starts out with a grid of unmarked squares." << endl;
+    	  // display instructions
+    	  cout << "Each Minesweeper game starts out with a grid of unmarked squares." << endl;
       	cout << "After clicking one of these squares, some of the squares will disappear," << endl;
       	cout << "some will remain blank, and some will have numbers on them." << endl;
       	cout << "It's your job to use the numbers to figure out which of the blank squares have mines and which are safe to click." << endl << endl;
@@ -84,7 +102,7 @@ void main_page(){
 
 int main(){
 
-	main_page();
+  main_page();
 
 	cout << endl << "Bye!" << endl;
 
