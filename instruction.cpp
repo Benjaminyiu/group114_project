de7a11f9aca3b22e77ae8834f5b5a19497ee567f
Benjaminyiu <kcyiuac@connect.hku.hk>
2021-04-26 16:40:35 +0800

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -9,6 +9,6 @@ void printInstructions() {
   cout << "After clicking one of these squares, some of the squares will disappear," << endl;
   cout << "some will remain blank, and some will have numbers on them." << endl;
   cout << "It's your job to use the numbers to figure out which of the blank squares have mines and which are safe to click." << endl << endl;
-  cout << "When inputting the coordinates, you must put the row index first and then column index (row index, column index)" << endl;
-  cout << "For instance, if user input is 1 1(separated by a space), it means starting from (0,0) moving by row index 1 and column index 1 "<< endl;
+  cout << "When inputting the coordinates, you must put the column index first and then row index (column index, row index)" << endl;
+  cout << "For instance, if user input is 2 1(separated by a space), it means starting from (0,0) moving by column index 2 and row index 1 "<< endl;
 }
